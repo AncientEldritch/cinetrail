e4c51d837d8eb2b0db599dbdf2c8dd60f6303dce
AncientEldritch <Padashuri@gmail.com>
2023-09-09 20:54:56 -0400

    lesson2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
+import Homepage from './pages/Homepage/Homepage'
 import ThemeContextProvider from "./contexts/ThemeContext"
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 function App() {
+
+  const apiKey = import.meta.env.VITE_API_KEY
+  const baseUrl = import.meta.env.VITE_BASE_URL
 
   return (
     <ThemeContextProvider>
       <BrowserRouter>
         <Header />
+        <Routes>
+          <Route path="/" element={<Homepage apiKey={apiKey} baseUrl={baseUrl} />} />
+        </Routes>
       </BrowserRouter>
     </ThemeContextProvider>
   );
